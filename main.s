29ea0486cee8a1045a0f6230c4507caacc7d5b80
ViviTYW <48053581+ViviTYW@users.noreply.github.com>
2019-03-20 22:07:09 +0800

    Add files via upload

--- main.s
+++ main.s
@@ -1,4 +1,4 @@
-.syntax unified
+yntax unified
 
 .word 0x20000100
 .word _start
@@ -6,21 +6,52 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	
+	//
+	//mov # to reg
+	//
+	movs	r1,	#1
+	movs	r2,	#2
+	movs	r3,	#3
+    movs    r4, #4
+	
+	//
+	//push
+	//
+	push	{r1, r2, r3, r4}
+
+    //
+	//pop
+	//
+	pop	    {r5, r6, r7, r8}
 
 	//
-	//branch w/o link
+	//clean the register data
 	//
-	b	label01
+	movs    r5, #0
+	movs    r6, #0
+	movs    r7, #0
+	movs    r8, #0
+
+	//
+	//push
+	//
+	push   {r4,r3,r2,r1}
+
+	//
+	//pop
+	//
+	pop    {r8, r7, r6, r5}
+
+	//
+	//b bl
+	//
+	bl	label01
+
+sleep:
+	b	sleep
 
 label01:
 	nop
-
-	//
-	//branch w/ link
-	//
-	bl	sleep
-
-sleep:
 	nop
-	b	.
+bx lr
